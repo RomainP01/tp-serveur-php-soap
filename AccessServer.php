a2fb4c9e700ed9d68cb5a56a4b07e01340db892a
Romain <romainpani01@gmail.com>
2022-04-21 15:10:42 +0200

    delete recipe method

--- AccessServer.php
+++ AccessServer.php
@@ -14,7 +14,7 @@ function getAllRecipes()
         die("Connection failed: " . $conn->connect_error);
     }
 
-    $sql = "SELECT id, nomRecette, descriptionRecette FROM recipe";
+    $sql = "SELECT id, nomRecette, descriptionRecette, ingredients FROM recipe";
     return $conn->query($sql)->fetch_all();
 }
 
@@ -32,8 +32,8 @@ function addRecipe($elements)
         die("Connection failed: " . $conn->connect_error);
     }
 
-    $sql = "INSERT INTO recipe (nomRecette, descriptionRecette)
-    VALUES ('$elements[0]', '$elements[1]')";
+    $sql = "INSERT INTO recipe (nomRecette, descriptionRecette, ingredients)
+    VALUES ('$elements[0]', '$elements[1]', '$elements[2]')";
 
     if ($conn->query($sql) === TRUE) {
         echo "New record created successfully";
@@ -65,10 +65,35 @@ function deleteRecipe($id)
     }
 }
 
+//function that modify the ingredients of a recipe
+function modifyRecipe($elements)
+{
+    $servername = "localhost";
+    $username = "root";
+    $password = "";
+    $dbname = "soap-kitchen";
+    $id = $elements[0];
+    // Create connection
+    $conn = new mysqli($servername, $username, $password, $dbname);
+    // Check connection
+    if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+    }
+
+    $sql = "UPDATE recipe SET ingredients = '$elements[1]' WHERE id = $id";
+
+    if ($conn->query($sql) === TRUE) {
+        echo "Record updated successfully";
+    } else {
+        echo "Error updating record: " . $conn->error;
+    }
+}
+
 ini_set('soap.wsdl_cache_enabled', 0);
 $serversoap = new SoapServer("http://localhost/tp-serveur-php-soap/server.wsdl");
 $serversoap->addFunction("getAllRecipes");
 $serversoap->addFunction("addRecipe");
 $serversoap->addFunction("deleteRecipe");
+$serversoap->addFunction("modifyRecipe");
 $serversoap->handle();
 ?>
--- Client.php
+++ Client.php
@@ -3,6 +3,7 @@ ini_set('soap.wsdl_cache_enabled', 0);
 $service = new SoapClient("http://localhost/tp-serveur-php-soap/server.wsdl");
 $allRecipes = $service->getAllRecipes();
 $deleteRecipe = $service->deleteRecipe(5);
-$addRecipe = $service->addRecipe(["welsh", "Un bon frometon sur son crouton"]);
+$addRecipe = $service->addRecipe(["welsh", "Un bon frometon sur son crouton", 'un pain et du fromage']);
+$modifyRecipe = $service->modifyRecipe([1,"un pain et du fromage tartinée à la moutarde"]);
 var_dump($allRecipes);
 ?>
